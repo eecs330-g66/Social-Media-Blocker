868e11706c5361dd3fb9476fcd8dbb7ec74d559d
Clarissa Qian <clarissaqianx@gmail.com>
2019-02-20 22:40:30 -0600

    storage

--- prototype/app.js
+++ prototype/app.js
@@ -1,15 +1,19 @@
-var start_block = [];
-var end_block = [];
+var start_block;
+var end_block;
 
 function init() {
-	
+	//console.log(start_block);
+	//document.getElementById('tues8').style.backgroundColor = 'blue';
+	var start = localStorage.getItem('start_block');
+	var end = localStorage.getItem('end_block');
+	console.log(start);
+	console.log(end);
 }
 
 function store() {
-	start_block = [document.getElementById('start_day').value, document.getElementById('start_hour').value];
-	end_block = [document.getElementById('end_day').value, document.getElementById('end_hour').value];
-	console.log(start_block);
-	console.log(end_block);
+	localStorage['start_block'] = document.getElementById('start_day').value + document.getElementById('start_hour').value;
+	console.log(localStorage);
+	localStorage['end_block'] = document.getElementById('end_day').value + document.getElementById('end_hour').value;
 }
 
 init();
